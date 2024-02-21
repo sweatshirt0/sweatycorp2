48d53852dcb6f5e2aff53d1947a5df4177f9a2d1
sweatshirt0 <sweatshirttt@proton.me>
2024-02-21 15:45:50 -0500

    hackmacs link added with server, image and css.

--- server/server.js
+++ server/server.js
@@ -4,101 +4,119 @@ const qs = require("querystring");
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const FP = path.join(process.env.HOME, 'sweatycorp2/pages/index.html');
-    const BP = path.join(process.env.HOME, 'sweatycorp2/pages/blog.html');
-    const FP2 = path.join(process.env.HOME, 'sweatycorp2/pages/info.html');
-    const FPS = path.join(process.env.HOME, 'sweatycorp2/style/style.css');
-    const FPI = path.join(process.env.HOME, 'sweatycorp2/style/newhara.jpg');
-    const FPI2 = path.join(process.env.HOME, 'sweatycorp2/style/rick.jpeg');
-    
-    if (req.url === "/") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	fs.readFile(FP, (error, data) => {
+  const FP = path.join(process.env.HOME, 'sweatycorp2/pages/index.html');
+  const BP = path.join(process.env.HOME, 'sweatycorp2/pages/blog.html');
+  const FP2 = path.join(process.env.HOME, 'sweatycorp2/pages/info.html');
+  const FPS = path.join(process.env.HOME, 'sweatycorp2/style/style.css');
+  const FPI = path.join(process.env.HOME, 'sweatycorp2/style/newhara.jpg');
+  const FPI2 = path.join(process.env.HOME, 'sweatycorp2/style/rick.jpeg');
+  const FPI3 = path.join(process.env.HOME, 'sweatycorp2/style/spacegliter.gif');
+  
+  if (req.url === "/") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/html");
+	  fs.readFile(FP, (error, data) => {
 	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
 	    } else {
-		res.write(data)
+		    res.write(data);
 	    }
 
 	    res.end();
-	});
-    }
+	  });
+  }
 
-    if (req.url === "/blog") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	fs.readFile(BP, (error, data) => {
+  if (req.url === "/blog") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/html");
+	  fs.readFile(BP, (error, data) => {
 	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
 	    } else {
-		res.write(data);
+		    res.write(data);
 	    }
 
 	    res.end();
-	});
-    }
+	  });
+  }
 
-    if (req.url === "/info") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	fs.readFile(FP2, (error, data) => {
+  if (req.url === "/info") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/html");
+	  fs.readFile(FP2, (error, data) => {
 	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
 	    } else {
-		res.write(data);
+		    res.write(data);
 	    }
 	    res.end();
-	});
-    }
+	  });
+  }
 
-    if (req.url === "/style") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/css");
-	fs.readFile(FPS, (error, data) => {
+  if (req.url === "/style") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/css");
+	  fs.readFile(FPS, (error, data) => {
 	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
 	    } else {
-		res.write(data);
-	    }
-
-	    res.end();
-	});
-    }
-
-    if (req.url === "/haru") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	fs.readFile(FPI, (error, data) => {
-	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
-	    } else {
-		res.write(data);
+		    res.write(data);
 	    }
 
 	    res.end();
-	});
-    }
+	  });
+  }
 
-    if (req.url === "/rick") {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/html");
-	fs.readFile(FPI2, (error, data) => {
+  if (req.url === "/haru") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/html");
+	  fs.readFile(FPI, (error, data) => {
 	    if (error) {
-		res.writeHead(404);
-		res.write("Error: File not found.");
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
 	    } else {
-		res.write(data);
+		    res.write(data);
 	    }
 
 	    res.end();
-	});
-    }
+	  });
+  }
+
+  if (req.url === "/rick") {
+	  res.statusCode = 200;
+	  res.setHeader("Content-Type", "text/html");
+	  fs.readFile(FPI2, (error, data) => {
+	    if (error) {
+		    res.writeHead(404);
+		    res.write("Error: File not found.");
+	    } else {
+		    res.write(data);
+	    }
+
+	    res.end();
+	  });
+
+  }
+
+  if (req.url === "/lore") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text.html");
+    fs.readFile(FPI3, (error, data) => {
+      if (error) {
+        res.writeHead(404);
+        res.write("Error: File not found.");
+      } else {
+        res.write(data);
+      }
+
+      res.end();
+    });
+  }
+
 });
 
 const port = process.env.port || 8080;
